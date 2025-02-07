17689761679dc7bdd4553eb6f06150f4f7bd6e99
braceal <abrace05@gmail.com>
2025-02-06 22:33:45 -0600

    evaluate median. 15B model

--- examples/pfam/evaluations/evaluation.py
+++ examples/pfam/evaluations/evaluation.py
@@ -353,6 +353,12 @@ if __name__ == '__main__':
         default='float32',
         help='The precision of the faiss index [float32, ubinary].',
     )
+    parser.add_argument(
+        '--gpus',
+        type=int,
+        default=0,
+        help='The number of GPUs to use for searching the faiss index.',
+    )
     args = parser.parse_args()
 
     # Get the dataset directory (a single directory named with a UUID
@@ -362,13 +368,20 @@ if __name__ == '__main__':
     # Will automatically create the faiss index if it does not exist
     faiss_index_path = args.model_dir / f'{args.precision}-faiss.index'
 
+    # The encoder model always gets placed on GPU:0 relative
+    # to CUDA_VISIBLE_DEVICES. If `gpus` > 0, then the faiss index
+    # will be placed on the next available GPUs (relative to
+    # CUDA_VISIBLE_DEVICES). Otherwise, the faiss index will share
+    # the same GPU as the encoder.
+    search_gpus = 0 if args.gpus == 0 else list(range(1, args.gpus + 1))
+
     # Initialize FaissIndexConfig
     faiss_config = FaissIndexConfig(
         dataset_dir=dataset_dir,
         faiss_index_path=faiss_index_path,
         precision=args.precision,
         search_algorithm='exact',
-        search_gpus=0,  # Defaults to GPU 0 as relative to CUDA_VISIBLE_DEVICES
+        search_gpus=search_gpus,
     )
 
     # Get the encoder configuration
