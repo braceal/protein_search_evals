1c8018657c11dd0b65f034203455b7b95b1bea5d
braceal <abrace05@gmail.com>
2026-02-15 22:50:19 -0600

    comment

--- protein_search_evals/evaluate.py
+++ protein_search_evals/evaluate.py
@@ -334,7 +334,7 @@ class Evaluator:
             accuracy_by_seq=accuracy_by_seq,
         )
 
-        # Compute the average sequence/cluster level mean accuracies
+        # Compute the average sequence/cluster level mean/median accuracies
         sequence_level_acc = self._compute_avg_accuracy(accuracy_by_seq)
         cluster_level_acc = self._compute_avg_accuracy(accuracy_by_cluster)
         cluster_level_median_acc = self._compute_median_accuracy(
